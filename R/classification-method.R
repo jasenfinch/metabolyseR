aa50ac4df6ecc275fa3ec80ecf8d8ef0364a33ff
jasenfinch <jsf9@aber.ac.uk>
2017-04-24 22:25:23 +0100

    classification parameters now added using formals

--- R/classification-method.R
+++ R/classification-method.R
@@ -4,30 +4,36 @@
 #' @importFrom parallel makeCluster parLapply clusterExport stopCluster
 
 setMethod("classification", signature = "Analysis",
-          function(object){
-            parameters <- object@parameters$classification
-            cls <- factor(object@preTreated$Info[,parameters$cls])
-            
-            par <- valipars(parameters$pars$sampling,parameters$pars$niter,parameters$pars$nreps,parameters$pars$strat,parameters$pars$div)
-            
-            dat.pair <- dat.sel1(object@preTreated$Data,cls,pwise = unique(as.character(cls)),pars = par)
-            com <- sapply(dat.pair, function(x){x$name})
+          function(x){
+            parameters <- x@parameters$classification
+            cls <- factor(x@preTreated$Info[,parameters$cls])
+            if (is.null(parameters$pars)) {
+              par <- valipars(parameters$pars$sampling,parameters$pars$niter,parameters$pars$nreps,parameters$pars$strat,parameters$pars$div)
+            } else {
+              newValipars <- valipars
+              pars <- formals(newValipars)
+              pars[names(parameters$pars)] <- parameters$pars
+              formals(newValipars) <- pars
+              par <- newValipars()
+            }
+            dat.pair <- dat.sel1(x@preTreated$Data,cls,pwise = unique(as.character(cls)),pars = par)
+            com <- sapply(dat.pair, function(y){y$name})
             if (length(com) > 1) {
               dat.pair <- dat.pair[-which(sapply(com,str_count,pattern = '~') > 1)]
             }
             clust = makeCluster(parameters$nCores, type = parameters$clusterType)
-            res.pair <- parLapply(clust,dat.pair,function(x,method,pars){
-              res.meth <- lapply(method,function(y,dat,pars){
-                accest(dat,clmeth = y,pars = pars)
-              },dat = x,pars = pars)
+            res.pair <- parLapply(clust,dat.pair,function(y,method,pars){
+              res.meth <- lapply(method,function(z,dat,pars){
+                accest(dat,clmeth = z,pars = pars)
+              },dat = y,pars = pars)
               names(res.meth) <- method
               return(res.meth)
             },method = parameters$method,pars = parameters$pars)
             stopCluster(clust)
-            names(res.pair) <- sapply(dat.pair, function(x){x$name})
+            names(res.pair) <- sapply(dat.pair, function(y){y$name})
             
-            object@classification <- res.pair
-            object@log$classification <- date()
-            return(object)
+            x@classification <- res.pair
+            x@log$classification <- date()
+            return(x)
           }
 )
