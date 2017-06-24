6a7aefb955875b32f2e35f39752b09cb52fbc4bb
Jasen <jsf9@aber.ac.uk>
2017-06-24 15:32:59 +0100

    removed documentation for methods

--- R/allGenerics.R
+++ R/allGenerics.R
@@ -1,22 +1,18 @@
-#' preTreat
 
 setGeneric("preTreat", function(x) {
   standardGeneric("preTreat")
 })
 
-#' classification
 
 setGeneric("classification", function(x) {
   standardGeneric("classification")
 })
 
-#' featureSelection
 
 setGeneric("featureSelection", function(x) {
   standardGeneric("featureSelection")
 })
 
-#' correlations
 
 setGeneric("correlations", function(x) {
   standardGeneric("correlations")
