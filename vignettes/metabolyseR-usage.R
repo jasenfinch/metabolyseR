505f69e98d2551c0803f272bef2566c120153a5d
jasenfinch <jsf9@aber.ac.uk>
2017-06-27 17:02:12 +0100

    added vignette

---
+++ vignettes/metabolyseR-usage.R
@@ -0,0 +1,6 @@
+## ----echo=FALSE----------------------------------------------------------
+  DiagrammeR::grViz(readr::read_file('metabolyseR.gv'))
+
+## ----echo=FALSE----------------------------------------------------------
+  DiagrammeR::grViz(readr::read_file('preTreat.gv'))
+
