6f44dd421f9136572900b30c91bb4ea0153fd656
jasenfinch <jsf9@aber.ac.uk>
2017-08-08 11:03:04 +0100

    occMat now counts NAs and 0s as unoccupied

--- R/occMat.R
+++ R/occMat.R
@@ -8,7 +8,7 @@ occMat <-
 			a <- cls == cls.1[i]
 			dat.1 <- dat[a,]
 			dat.1[dat.1 > 0] <- TRUE
-			dat.1[dat.1 == 0] <- FALSE
+			dat.1[dat.1 == 0 | is.na(dat.1)] <- FALSE
 			tot <- apply(dat.1,2,sum)
 			tot <- tot/nrow(dat.1)
 			mat[i,] <- tot
