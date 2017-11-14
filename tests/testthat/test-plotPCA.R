4b0bf7047bf05520ae69aa565b849189c2c97ea7
Jasen <jsf9@aber.ac.uk>
2017-11-14 17:50:48 +0000

    added tests for plotPCA and plotLDA

--- tests/testthat/test-plotPCA.R
+++ tests/testthat/test-plotPCA.R
@@ -1,24 +1,14 @@
 suppressPackageStartupMessages(library(FIEmspro))
 
-context('show')
+context('plotPCA')
 
-test_that('AnalysisParameters show method works',{
-  p <- analysisParameters()
-  out <- capture.output(show(p))
-  
-  expect_true(length(out) == 54)
-})
-
-test_that('Analysis show method works',{
+test_that('plotPCA returns a plot',{
   data(abr1)
-  p <- analysisParameters()
+  p <- analysisParameters(c('preTreat'))
   p@preTreat <- list(
     occupancyFilter = list(maximum = list()),
     transform = list(TICnorm = list())
   )
-  p@classification$nCores <- 1
-  p@featureSelection$nCores <- 1
-  p@featureSelection$pars$fs.rf$nreps <- 1
   cls1 <- abr1$neg[abr1$fact$class %in% c('1'),190:200][1:10,]
   cls2 <- abr1$neg[abr1$fact$class %in% c('6'),190:200][1:10,]
   dat <- rbind(cls1,cls2)
@@ -26,7 +16,7 @@ test_that('Analysis show method works',{
   inf2 <- abr1$fact[abr1$fact$class %in% c('6'),][1:10,]
   info <- rbind(inf1,inf2)
   analysis <- metabolyseR::metabolyse(dat,info,p)
-  out <- capture.output(show(analysis))
+  pl <- metabolyseR::plotPCA(analysis)
   
-  expect_true(length(out) == 26)
+  expect_false(F %in% (class(pl) == c('gg','ggplot')))
 })
