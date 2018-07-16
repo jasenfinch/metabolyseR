d6be97b907c148c862ba1aa401fcb9347ac9ed92
jasenfinch <jsf9@aber.ac.uk>
2018-07-16 13:32:22 +0100

    added skeleton for batch correction methods

---
+++ R/correctionMethods.R
@@ -0,0 +1,21 @@
+
+correctionMethods <- function(method = NULL, description = F){
+  methods <- list()
+  
+  descriptions <- list()
+  
+  if (description == F) {
+    if (is.null(method)) {
+      method <- methods
+    } else {
+      method <- methods[[method]]
+    }
+  } else {
+    if (is.null(method)) {
+      method <- descriptions
+    } else {
+      method <- descriptions[[method]]
+    }
+  }
+  return(method)
+}
