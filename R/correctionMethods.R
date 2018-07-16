114cf931406ca3ad14409339d1258d8c37bb58a6
jasenfinch <jsf9@aber.ac.uk>
2018-07-16 16:22:38 +0100

    added center correction method

--- R/correctionMethods.R
+++ R/correctionMethods.R
@@ -1,6 +1,30 @@
 
 correctionMethods <- function(method = NULL, description = F){
-  methods <- list()
+  methods <- list(
+    center = function(dat, cls = 'block', type = 'median'){
+      method <- get(type)
+      batches <- dat$Info[,cls] %>% unlist()
+      dat$Data <- dat$Data %>%
+        map_df(~{
+          d <- .
+          batchMeans <- tibble(Intensity = d,batch = batches) %>%
+            group_by(batch) %>%
+            mutate(Intensity = Intensity) %>%
+            summarise(Median = method(Intensity)) %>%
+            mutate(correction = Median - method(Median))
+          
+          correct <- tibble(Intensity = d,batch = batches) %>%
+            left_join(batchMeans, by = "batch") %>%
+            mutate(Intensity = Intensity) %>%
+            mutate(Intensity = Intensity - correction) %>%
+            dplyr::select(-correction,-Median,-batch) %>%
+            unlist()
+          correct[correct < 0] <- 0
+          return(correct)
+        })
+      return(dat)
+    }
+  )
   
   descriptions <- list()
   
