e83ddefbe3797c6dafef91125ed96d928b344872
jasenfinch <jsf9@aber.ac.uk>
2019-07-24 16:22:57 +0100

    ttest method now works

--- R/univariateMethods.R
+++ R/univariateMethods.R
@@ -4,7 +4,7 @@
 #' @export
 
 setMethod('ttest',signature = 'AnalysisData',
-          function(x,cls,pAdjust = 'bonferroni'){
+          function(x,cls,pAdjust = 'bonferroni', returnModels = F, ...){
             
             d <- x %>%
               dat()
@@ -18,24 +18,59 @@ setMethod('ttest',signature = 'AnalysisData',
             pw <- classes %>%
               map(pairwises)
             
-            res <- pw %>%
+            models <- pw %>%
+              names() %>%
               map(~{
                 cl <- .
-                cl %>%
+                pw[[cl]] %>%
                   map(~{
                     p <- .  
+                    pc <- str_split(p,'~')[[1]]
                     
-                  })
-              })
-            
-            
-            d %>%
-              map_df(~{
-                r <- t.test(. ~ cls)
-                return(tibble(Score = r$statistic,Pvalue = r$p.value))
+                    pad <- removeClasses(x,cl,classes = info(x) %>%
+                                           select(cl) %>%
+                                           unlist() %>%
+                                           unique() %>%
+                                           .[!(. %in% pc)])
+                    
+                    response <- pad %>%
+                      info() %>%
+                      select(cl) %>%
+                      unlist() %>%
+                      factor()
+                    
+                    pad %>%
+                      dat() %>%
+                      map(~{
+                        t.test(. ~ response)
+                      })
+                  }) %>%
+                set_names(pw[[cl]])
               }) %>%
-              set_names(colnames(d)) %>%
-              mutate(adjustedPvalue = p.adjust(Pvalue,method = pAdjust))
-            return(res)
+              set_names(names(pw))
+            
+            results <- models %>%
+              map(~{
+                map(.,~{
+                  map(.,glance) %>%
+                    bind_rows(.id = 'Feature') %>%
+                    mutate(adjusted.p.value = p.adjust(p.value,method = pAdjust))
+                }) %>%
+                  bind_rows(.id = 'Pairwise')
+              }) %>%
+              bind_rows(.id = 'Predictor')
+              
           }
 )
+
+
+#             d %>%
+#               map_df(~{
+#                 r <- t.test(. ~ cls)
+#                 return(tibble(Score = r$statistic,Pvalue = r$p.value))
+#               }) %>%
+#               set_names(colnames(d)) %>%
+#               mutate(adjustedPvalue = p.adjust(Pvalue,method = pAdjust))
+#             return(res)
+#           }
+# )
