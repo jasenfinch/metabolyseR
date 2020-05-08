0e6e0d42837a8ce97e5cb5833b77f89587e2f615
jasenfinch <jsf9@aber.ac.uk>
2020-05-08 17:01:08 +0100

    added parameters tests

---
+++ tests/testthat/test-parameters.R
@@ -0,0 +1,41 @@
+library(metaboData)
+
+context('parameters')
+
+p <- analysisParameters()
+a <- new('Analysis')
+
+test_that('parameters assigned for Analysis objects',{
+  parameters(a) <- p
+  pa <- a %>%
+    parameters() %>%
+    parameters('pre-treatment')
+  expect_equal(length(pa),length(parameters(p,'pre-treatment')))
+})
+
+test_that('parameters extracted for Analysis objects',{
+  parameters(a) <- p
+  expect_s4_class(parameters(a),'AnalysisParameters')
+})
+
+test_that('parameters extracted for AnalysisParameters objects',{
+  params <- parameters(p,'pre-treatment')
+  expect_length(params,4)
+})
+
+test_that('pre-treatment parameters assigned for AnalysisParameters',{
+  parameters(p,'pre-treatment') <- preTreatmentParameters(list(transform = 'TICnorm'))
+  expect_identical(parameters(p,'pre-treatment') %>% names(),'transform')
+})
+
+test_that('modelling parameters assigned for AnalysisParameters',{
+  parameters(p,'modelling') <- modellingParameters('anova')
+  expect_identical(parameters(p,'modelling') %>% names(),'anova')
+})
+
+test_that('correlations parameters assigned for AnalysisParameters',{
+  parameters(p,'correlations') <- correlationsParameters()
+  expect_identical(parameters(p,'correlations') %>% 
+                     names(),
+                   c("method","pAdjustMethod","corPvalue"))
+})
