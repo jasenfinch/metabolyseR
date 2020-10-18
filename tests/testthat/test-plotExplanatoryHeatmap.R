63c1a7330680eb8e1f77777a4c08b746406ea8bb
jasenfinch <jsf9@aber.ac.uk>
2020-10-19 00:54:21 +0100

    fixed plotExplanatoryHeatmap tests

--- tests/testthat/test-plotExplanatoryHeatmap.R
+++ tests/testthat/test-plotExplanatoryHeatmap.R
@@ -3,33 +3,22 @@ library(metaboData)
 context('plotExplanatoryHeatmap')
 
 test_that('plotExplanatoryHeatmap returns a plot',{
-  p <- analysisParameters(c('pre-treatment','modelling'))
   
-  parameters(p,"pre-treatment") <- list(
-    occupancyFilter = list(maximum = list()),
-    transform = list(TICnorm = list())
-  )
+  pl <- analysisData(abr1$neg,abr1$fact) %>%
+    keepFeatures(features = features(.)[190:220]) %>%
+    keepClasses(classes = c('1','6')) %>%
+    randomForest(cls = 'day',nCores = 1) %>%
+    plotExplanatoryHeatmap(threshold = 0.5)
   
-  parameters(p,'modelling')$randomForest$nCores <- 1
-  parameters(p,'modelling')$randomForest$cls <- 'day'
-
-  cls1 <- abr1$neg[abr1$fact$class %in% c('1'),190:220][1:10,]
-  cls2 <- abr1$neg[abr1$fact$class %in% c('6'),190:220][1:10,]
-  dat <- rbind(cls1,cls2)
-  inf1 <- abr1$fact[abr1$fact$class %in% c('1'),][1:10,]
-  inf2 <- abr1$fact[abr1$fact$class %in% c('6'),][1:10,]
-  info <- rbind(inf1,inf2)
-  analysis <- metabolyse(dat,info,p,verbose = F)
-  pl <- plotExplanatoryHeatmap(analysis,threshold = 0.5)
-  
-  expect_true(class(pl) == 'list')
+  expect_identical(class(pl),c("patchwork","plot_filler","gg","ggplot"))
 })
 
 test_that('plotExplanatoryHeatmap returns a plot',{
-  d <- analysisData(abr1$neg[,200:250],abr1$fact)
-  rf <- randomForest(d,cls = 'injorder',perm = 3,nCores = 2)
+  d <- analysisData(abr1$neg,abr1$fact) %>%
+    keepFeatures(features = features(.)[200:250])
+  x <- randomForest(d,cls = 'injorder',perm = 3,nCores = 2)
   
-  pl <- plotExplanatoryHeatmap(rf$injorder,measure = 'IncNodePurity')
+  pl <- plotExplanatoryHeatmap(x,metric = 'IncNodePurity')
   
   expect_identical(class(pl),c("patchwork","plot_filler","gg","ggplot"))
 })
