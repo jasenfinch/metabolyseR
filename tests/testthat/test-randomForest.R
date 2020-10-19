4ed2d4b2618d67bc18ca04cc43530c6d88be92c8
jasenfinch <jsf9@aber.ac.uk>
2020-10-19 01:03:51 +0100

    fixed random forest tests

--- tests/testthat/test-randomForest.R
+++ tests/testthat/test-randomForest.R
@@ -1,23 +1,22 @@
 library(metaboData)
 
-data("abr1")
-
-d <- analysisData(abr1$neg[,200:250],abr1$fact)
+d <- analysisData(abr1$neg,abr1$fact) %>%
+  keepFeatures(features = features(.)[200:250])
 
 context('randomForest')
 
 test_that('unsupervised random forest works',{
   rf <- randomForest(d,cls = NULL,nCores = 2)
   
-  expect_true(is.list(rf))
-  expect_true(class(rf[[1]]) == 'RandomForest')
+  expect_s4_class(rf,'RandomForest')
+  expect_identical(type(rf),'unsupervised')
 })
 
 test_that('random forest classification works',{
   rf <- randomForest(d,cls = 'day',perm = 3,nCores = 2,returnModels = TRUE)
 
-  expect_true(is.list(rf))
-  expect_true(class(rf$day) == 'RandomForest')
+  expect_s4_class(rf,'RandomForest')
+  expect_identical(type(rf),'classification')
 })
 
 test_that('binary classification works',{
@@ -26,13 +25,12 @@ test_that('binary classification works',{
   
   rf <- randomForest(d,cls = 'day',nCores = 2,binary = TRUE)
   
-  expect_true(is.list(rf))
-  expect_true(class(rf$day) == 'RandomForest')
+  expect_s4_class(rf,'RandomForest')
 })
 
 test_that('random forest regression works',{
   rf <- randomForest(d,cls = 'injorder',perm = 3,nCores = 2)
   
-  expect_true(is.list(rf))
-  expect_true(class(rf[[1]]) == 'RandomForest')
+  expect_s4_class(rf,'RandomForest')
+  expect_identical(type(rf),'regression')
 })
