714b4df0cbc923ab13aad51576ff2ecc45412a38
jasenfinch <jsf9@aber.ac.uk>
2020-10-30 17:00:06 +0000

    random forest mostly tested

--- tests/testthat/test-randomForest.R
+++ tests/testthat/test-randomForest.R
@@ -6,17 +6,26 @@ d <- analysisData(abr1$neg,abr1$fact) %>%
 context('randomForest')
 
 test_that('unsupervised random forest works',{
-  rf <- randomForest(d,cls = NULL,nCores = 2)
+  rf <- randomForest(d,cls = NULL,nCores = 2,returnModels = TRUE)
   
   expect_s4_class(rf,'RandomForest')
   expect_identical(type(rf),'unsupervised')
 })
 
 test_that('random forest classification works',{
-  rf <- randomForest(d,cls = 'day',perm = 3,nCores = 2,returnModels = TRUE)
-
-  expect_s4_class(rf,'RandomForest')
-  expect_identical(type(rf),'classification')
+  rf <- randomForest(d,cls = c('day','name'),perm = 3,nCores = 2,returnModels = TRUE)
+  
+  rf_metrics <- metrics(rf)
+  rf_importance <- importance(rf)
+  
+  rf_wrong <- c(rf,list('wrong'))
+  
+  expect_identical(class(rf),'list')
+  expect_identical(type(rf$day),'classification')
+  expect_s3_class(rf_metrics,'tbl_df')
+  expect_s3_class(rf_importance,'tbl_df')
+  expect_error(metrics(rf_wrong))
+  expect_error(importance(rf_wrong))
 })
 
 test_that('binary classification works',{
@@ -29,8 +38,23 @@ test_that('binary classification works',{
 })
 
 test_that('random forest regression works',{
-  rf <- randomForest(d,cls = 'injorder',perm = 3,nCores = 2)
+  rf <- randomForest(d,cls = 'injorder',perm = 3,nCores = 2,returnModels = TRUE)
   
   expect_s4_class(rf,'RandomForest')
   expect_identical(type(rf),'regression')
+})
+
+test_that('low sample permutation testing works',{
+  rf <- analysisData(data = abr1$neg,info = abr1$fact) %>%
+    keepSamples(idx = 'injorder',samples = 1:2) %>%
+    randomForest(cls = 'injorder',perm = 1000)
+  
+  expect_s4_class(rf,'RandomForest')
+})
+
+test_that('specifiying comparisons works',{
+  rf <- randomForest(d,comparisons = list(day = 'H~5'),
+                     cls = 'day')
+  
+  expect_s4_class(rf,'RandomForest')
 })
