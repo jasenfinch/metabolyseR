7023a1d4476fb2a2dbc2474a2a7feb131f40afda
jasenfinch <jsf9@aber.ac.uk>
2021-04-15 02:28:50 +0100

    added further tests for correlations

--- tests/testthat/test-correlations.R
+++ tests/testthat/test-correlations.R
@@ -5,10 +5,27 @@ test_that('correlations-works', {
   p <- analysisParameters(elements = c('correlations'))
   changeParameter(p,'method') <- 'spearman'
   
-  d <- metabolyse(abr1$neg[,200:300],abr1$fact,p,verbose = FALSE) %>%
+  d <- metabolyse(abr1$neg[,200:300],abr1$fact,p) %>%
     analysisResults('correlations')
   
   expect_s3_class(d,'tbl_df')
 })
 
+test_that('correlations errors when incorrect method specified',{
+  d <- analysisData(abr1$neg[,200:250],abr1$fact)
+  
+  expect_error(correlations(d,method = 'wrong'))
+})
+
+test_that('correlations errors when incorrect p value adjustment method specified',{
+  d <- analysisData(abr1$neg[,200:250],abr1$fact)
+  
+  expect_error(correlations(d,pAdjustMethod = 'wrong'))
+})
+
+test_that('correlations errors when incorrect correlation p value threshold specified',{
+  d <- analysisData(abr1$neg[,200:250],abr1$fact)
+  
+  expect_error(correlations(d,corPvalue = 'wrong'))
+})
 
