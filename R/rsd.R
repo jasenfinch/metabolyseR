c3b80674c957db99e6000b7d0747694adf3260d5
jasenfinch <jsf9@aber.ac.uk>
2021-04-16 00:01:28 +0100

    rsd now returns mean and SD. Added example to rsd documentation

--- R/rsd.R
+++ R/rsd.R
@@ -1,10 +1,23 @@
-#' rsd
+#' Calculate feature relative standard deviations
 #' @rdname rsd
-#' @description Calculate relative standard deviation (RSD) percent values for each 
-#' feature per class for a given info column.
-#' @param x S4 object of class AnalysisData
-#' @param cls info column to use for class structure
+#' @description Calculate relative standard deviation (RSD) percentage values for each 
+#' feature per class for a given sample information column.
+#' @param x S4 object of class `AnalysisData`
+#' @param cls sample information column to use for class structure
+#' @return A tibble containing the computed RSD values.
+#' @examples 
+#' library(metaboData)
+#' 
+#' d <- analysisData(abr1$neg[,200:300],abr1$fact)
+#' 
+#' rsd(d,cls = 'day')
 #' @export
+
+setGeneric("rsd", function(x,cls = 'class') {
+  standardGeneric("rsd")
+})
+
+#' @rdname rsd
 
 setMethod('rsd',signature = 'AnalysisData',
           function(x,cls = 'class'){
@@ -16,6 +29,8 @@ setMethod('rsd',signature = 'AnalysisData',
               mutate(Class = clsExtract(x,cls)) %>%
               gather(Feature,Intensity,-Class) %>%
               group_by(Class,Feature) %>%
-              summarise(RSD = sd(Intensity)/mean(Intensity) * 100,.groups = 'drop') %>%
+              summarise(Mean = mean(Intensity),
+                        SD = sd(Intensity),
+                        RSD = SD/Mean * 100,.groups = 'drop') %>%
               rename(!!vars)
           })
