f29d6518304b5ef9b7549cf252e37b987965abed
jasenfinch <jsf9@aber.ac.uk>
2022-10-27 23:41:43 +0100

    move unsupervised RF code to own file

--- R/randomForest-internals.R
+++ R/randomForest-internals.R
@@ -2,8 +2,10 @@
 performRF <- function(x,cls,rf,type,returnModel){
   params <- formals(randomForest::randomForest)
   params$x <- x
-  params$y <- cls
   params <- c(params,rf)
+  
+  if (!is.null(cls)) params$y <- cls
+  
   model <- do.call(randomForest::randomForest,params)
   
   model_results <- list(metrics = performanceMetrics(model,
@@ -52,66 +54,6 @@ collate <- function(models,results,type){
          classification = collateClassification(models,results),
          regression = collateRegression(models,results)
   )
-}
-
-#' @importFrom forestControl fpr_fs
-
-unsupervised <- function(x,rf,reps,returnModels,seed,...){
-  
-  set.seed(seed)
-  
-  models <- future_map(1:reps,~{
-    params <- formals(randomForest::randomForest)
-    params$x <- x %>% dat()
-    params <- c(params,rf)
-    do.call(randomForest::randomForest,params)
-  },.options = furrr_options(seed = seed)) %>%
-    set_names(1:reps)
-  
-  importances <- models %>%
-    future_map_dfr(~{
-      m <- .
-      importance(m) %>%
-        left_join(fpr_fs(m),by = c('Feature' = 'variable')) %>%
-        rename(SelectionFrequency = freq,FalsePositiveRate = fpr)
-    },
-    .id = 'Rep',
-    .options = furrr_options(seed = seed)) %>%
-    mutate(Rep = as.numeric(Rep)) %>%
-    gather('Metric','Value',-Rep,-Feature)
-  
-  proximities <- models %>%
-    future_map_dfr(.,~{.$proximity %>%
-        as_tibble(.name_repair = 'minimal') %>%
-        mutate(Sample = seq_len(nrow(.))) %>%
-        gather('Sample2','Proximity',-Sample) %>%
-        rename(Sample1 = Sample)
-    },
-    .id = 'Rep',
-    .options = furrr_options(seed = seed)) %>%
-    mutate(Rep = as.numeric(Rep))  %>%
-    mutate(Sample2 = as.numeric(Sample2))
-  
-  results <- list(
-    importances = importances %>%
-      select(-Rep) %>%
-      group_by(Feature,Metric) %>%
-      summarise(Value = mean(Value))
-  )
-  
-  res <- new('RandomForest')
-  res@type <- 'unsupervised'
-  dat(res) <- dat(x)
-  sinfo(res) <- sinfo(x)
-  res@results <- results
-  res@importances <- importances
-  res@proximities <- proximities
-  
-  if (isTRUE(returnModels)) {
-    res@models <- models
-  }
-  
-  return(list(res))  
 }
 
 supervised <- function(x,
