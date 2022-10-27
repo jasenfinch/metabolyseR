72281fca5d40d3e9e8bd88ef4140769dee73660f
jasenfinch <jsf9@aber.ac.uk>
2022-10-27 23:59:52 +0100

    streamline unsupervised random forest

--- R/randomForest-unsupervised.R
+++ R/randomForest-unsupervised.R
@@ -1,58 +1,40 @@
 
-unsupervised <- function(x,rf,reps,returnModels,seed,...){
-  
-  set.seed(seed)
+collateUnsupervised <- function(models,result){
+  models %>% 
+    map_dfr(~.x[[result]],
+            .id = 'rep')
+}
+
+unsupervised <- function(x,
+                         rf = list(),
+                         reps = 1,
+                         returnModels = FALSE,
+                         seed = 1234,
+                         ...){
   
   models <- future_map(1:reps,~{
-    params <- formals(randomForest::randomForest)
-    params$x <- x %>% dat()
-    params <- c(params,rf)
-    do.call(randomForest::randomForest,params)
+    performRF(
+      dat(x),
+      cls = NULL,
+      rf = rf,
+      type = 'unsupervised',
+      returnModel = returnModels
+    )
   },.options = furrr_options(seed = seed)) %>%
     set_names(1:reps)
   
-  importances <- models %>%
-    future_map_dfr(~{
-      m <- .
-      importance(m) %>%
-        left_join(fpr_fs(m),by = c('Feature' = 'variable')) %>%
-        rename(SelectionFrequency = freq,FalsePositiveRate = fpr)
-    },
-    .id = 'Rep',
-    .options = furrr_options(seed = seed)) %>%
-    mutate(Rep = as.numeric(Rep)) %>%
-    gather('Metric','Value',-Rep,-Feature)
+  res <- new('RandomForest',
+             x,
+             type = 'unsupervised',
+             importances = collate(models,'importance',type = 'unsupervised') %>% 
+               group_by(feature,metric) %>% 
+               summarise(value = mean(value)),
+             proximities = collate(models,'proximities',type = 'unsupervised'))
   
-  proximities <- models %>%
-    future_map_dfr(.,~{.$proximity %>%
-        as_tibble(.name_repair = 'minimal') %>%
-        mutate(Sample = seq_len(nrow(.))) %>%
-        gather('Sample2','Proximity',-Sample) %>%
-        rename(Sample1 = Sample)
-    },
-    .id = 'Rep',
-    .options = furrr_options(seed = seed)) %>%
-    mutate(Rep = as.numeric(Rep))  %>%
-    mutate(Sample2 = as.numeric(Sample2))
-  
-  results <- list(
-    importances = importances %>%
-      select(-Rep) %>%
-      group_by(Feature,Metric) %>%
-      summarise(Value = mean(Value))
-  )
-  
-  res <- new('RandomForest')
-  res@type <- 'unsupervised'
-  dat(res) <- dat(x)
-  sinfo(res) <- sinfo(x)
-  res@results <- results
-  res@importances <- importances
-  res@proximities <- proximities
   
   if (isTRUE(returnModels)) {
-    res@models <- models
+    res@models <- collateModels(models)
   }
   
-  return(list(res))  
+  return(res)  
 }
