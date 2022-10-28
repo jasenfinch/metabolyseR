7420218a7200cc74e67c06adaeb7ae80d1fea5cc
jasenfinch <jsf9@aber.ac.uk>
2022-10-28 11:18:57 +0100

    remove metaboData library load from metabolyse tests

--- tests/testthat/test-metabolyse.R
+++ tests/testthat/test-metabolyse.R
@@ -1,6 +1,3 @@
-library(metaboData)
-
-context('metabolyse')
 
 test_that('metabolyse-works', {
   p <- analysisParameters()
@@ -13,12 +10,14 @@ test_that('metabolyse-works', {
   
   changeParameter(p,'cls') <- 'day'
   
-  cls1 <- abr1$neg[abr1$fact$class %in% c('1'),190:200][1:10,]
-  cls2 <- abr1$neg[abr1$fact$class %in% c('6'),190:200][1:10,]
+  cls1 <- metaboData::abr1$neg[metaboData::abr1$fact$class %in% c('1'),190:200][1:10,]
+  cls2 <- metaboData::abr1$neg[metaboData::abr1$fact$class %in% c('6'),190:200][1:10,]
   dat <- rbind(cls1,cls2)
-  inf1 <- abr1$fact[abr1$fact$class %in% c('1'),][1:10,]
-  inf2 <- abr1$fact[abr1$fact$class %in% c('6'),][1:10,]
+  
+  inf1 <- metaboData::abr1$fact[abr1$fact$class %in% c('1'),][1:10,]
+  inf2 <- metaboData::abr1$fact[abr1$fact$class %in% c('6'),][1:10,]
   info <- rbind(inf1,inf2)
+  
   analysis <- metabolyse(dat,info,p,verbose = FALSE)
 
   expect_true(isS4(analysis))
